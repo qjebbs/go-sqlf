0209117b9f12419dad8467b52da8497f88b6c1b7
jebbs <qjebbs@gmail.com>
2024-02-17 09:13:07 +0800

    remove dependency of go-cmp

--- syntax/parser_test.go
+++ syntax/parser_test.go
@@ -1,17 +1,17 @@
-package syntax_test
+package syntax
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
-	"github.com/qjebbs/go-sqlf/syntax"
 )
 
 func TestParser(t *testing.T) {
+	newExpr := func(line, col uint) expr {
+		return expr{node{Pos{line, col}}}
+	}
 	testCases := []struct {
 		raw     string
-		want    []syntax.Expr
+		want    []Expr
 		wantErr bool
 	}{
 		{
@@ -28,54 +28,47 @@ func TestParser(t *testing.T) {
 		},
 		{
 			raw: "?,?,?",
-			want: []syntax.Expr{
-				&syntax.BindVarExpr{Type: syntax.Question, Index: 1},
-				&syntax.PlainExpr{Text: ","},
-				&syntax.BindVarExpr{Type: syntax.Question, Index: 2},
-				&syntax.PlainExpr{Text: ","},
-				&syntax.BindVarExpr{Type: syntax.Question, Index: 3},
+			want: []Expr{
+				&BindVarExpr{Type: Question, Index: 1, expr: newExpr(1, 1)},
+				&PlainExpr{Text: ",", expr: newExpr(1, 2)},
+				&BindVarExpr{Type: Question, Index: 2, expr: newExpr(1, 3)},
+				&PlainExpr{Text: ",", expr: newExpr(1, 4)},
+				&BindVarExpr{Type: Question, Index: 3, expr: newExpr(1, 5)},
 			},
 		},
 		{
 			raw: "$1'#c11#t111#fragment1111'",
-			want: []syntax.Expr{
-				&syntax.BindVarExpr{Type: syntax.Dollar, Index: 1},
-				&syntax.PlainExpr{Text: "'#c11#t111#fragment1111'"},
+			want: []Expr{
+				&BindVarExpr{Type: Dollar, Index: 1, expr: newExpr(1, 1)},
+				&PlainExpr{Text: "'#c11#t111#fragment1111'", expr: newExpr(1, 3)},
 			},
 		},
 		{
 			raw: "#join('#c=#argDollar', ',')",
-			want: []syntax.Expr{
-				&syntax.FuncCallExpr{
+			want: []Expr{
+				&FuncCallExpr{
 					Name: "join",
 					Args: []any{"#c=#argDollar", ","},
+					expr: newExpr(1, 1),
 				},
 			},
 		},
 		{
 			raw: "#c1#t1#fragment1",
-			want: []syntax.Expr{
-				&syntax.FuncCallExpr{Name: "c", Args: []any{float64(1)}},
-				&syntax.FuncCallExpr{Name: "t", Args: []any{float64(1)}},
-				&syntax.FuncCallExpr{Name: "fragment", Args: []any{float64(1)}},
+			want: []Expr{
+				&FuncCallExpr{Name: "c", Args: []any{float64(1)}, expr: newExpr(1, 1)},
+				&FuncCallExpr{Name: "t", Args: []any{float64(1)}, expr: newExpr(1, 4)},
+				&FuncCallExpr{Name: "fragment", Args: []any{float64(1)}, expr: newExpr(1, 7)},
 			},
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.raw, func(t *testing.T) {
-			got, err := syntax.Parse(tc.raw)
+			got, err := Parse(tc.raw)
 			if !tc.wantErr && err != nil {
 				t.Fatal(err)
 			}
-			if !tc.wantErr && !cmp.Equal(
-				got.ExprList, tc.want,
-				cmpopts.IgnoreUnexported(
-					syntax.PlainExpr{},
-					syntax.BindVarExpr{},
-					syntax.FuncExpr{},
-					syntax.FuncCallExpr{},
-				),
-			) {
+			if !tc.wantErr && !reflect.DeepEqual(got.ExprList, tc.want) {
 				for _, tk := range got.ExprList {
 					t.Logf("%#v", tk)
 				}
