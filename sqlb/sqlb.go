7267cbbb1018f7edd3aaf7a1e4a7ffa7dd581273
jebbs <qjebbs@gmail.com>
2025-08-26 14:58:40 +0800

    Update sqlb.Builder

--- sqlb/sqlb.go
+++ sqlb/sqlb.go
@@ -4,14 +4,11 @@
 package sqlb
 
 import (
-	"github.com/qjebbs/go-sqlf/v3"
 	"github.com/qjebbs/go-sqlf/v3/syntax"
 )
 
 // Builder is the interface for sql builders.
 type Builder interface {
-	sqlf.Builder
-
 	// BuildQuery builds and returns the query and args.
 	BuildQuery(bindVarStyle syntax.BindVarStyle) (query string, args []any, err error)
 }
